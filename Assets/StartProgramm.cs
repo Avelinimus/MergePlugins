05fbcc76336b80967b4f0043bf0684dea81d36f8
Avelinimus <avel63146@gmail.com>
2023-01-20 17:26:46 +0200

    Notification

--- Assets/StartProgramm.cs
+++ Assets/StartProgramm.cs
@@ -1,4 +1,5 @@
 using Firebase.Storage;
+using Unity.Notifications.Android;
 using UnityEngine;
 
 public class StartProgramm : MonoBehaviour
@@ -6,5 +7,21 @@ public class StartProgramm : MonoBehaviour
     void Start()
     {
         FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+        var notification = new AndroidNotification();
+        notification.Title = "Your Title";
+        notification.Text = "Your Text";
+        notification.FireTime = System.DateTime.Now.AddMinutes(1);
+
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
     }
 }
